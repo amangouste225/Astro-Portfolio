4dac367424b0d4ad7607ab69ce8b6550d1f2bec2
Amangoua Ange <adagou@yahoo.com>
2023-12-26 10:13:09 +0000

    building hero

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1436px",
+        "3xl": "1600px",
+      },
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+        display: ["Caveat", "ui-sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
