a693a04bb0c891701db2a1005c584a6c340e13c4
Amangoua Ange <adagou@yahoo.com>
2023-12-27 20:07:20 +0000

    accessibility

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,10 +4,8 @@ export const collections = {
   work: defineCollection({
     schema: z.object({
       title: z.string(),
-      tools: z.array(z.string()),
       img: z.string(),
       img_alt: z.string().optional(),
-      name: z.string(),
     }),
   }),
 };
