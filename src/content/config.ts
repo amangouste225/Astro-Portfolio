4ce00b78e461cbf8f6c0ba863a82800075d625ee
Amangoua Ange <adagou@yahoo.com>
2023-12-27 19:28:02 +0000

    works section completed

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,6 @@ import { defineCollection, z } from "astro:content";
 
 export const collections = {
   work: defineCollection({
-    type: "content",
     schema: z.object({
       title: z.string(),
       tools: z.array(z.string()),
