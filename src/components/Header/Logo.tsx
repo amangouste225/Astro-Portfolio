69f30b1a327b02755454fd10c719c9adad8dac67
Amangoua Kacou Ange-Thierry <104840429+amangouste225@users.noreply.github.com>
2024-06-19 15:02:13 +0000

    dark mode

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <a
       href="/"
-      className="flex items-center gap-1 lg:mt-10 px-10 text-sm dark:text-neutral-50 text-neutral-900"
+      className="flex items-center gap-2 lg:mt-10 px-5 text-sm dark:text-neutral-50 text-neutral-100 lg:text-neutral-900"
     >
       <div className="w-9 h-9 rounded-full border border-neutral-700 bg-[url('/mike.jpg')] bg-center bg-contain "></div>
       <div className="flex flex-col gap-0">
